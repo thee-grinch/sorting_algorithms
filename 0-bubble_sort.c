61a113d77b37553505dc897318f4fc79de760ce4
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-22 21:48:15 +0300

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 	}
 	else
 	{
-		for (i = 0; i <  size; i++)
+		for (i = 0; i <  size - 1; i++)
 		{
 			sorted = 0;
 			for (j = 0; j < size - 1 - i; j++)
