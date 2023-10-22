e0fe5456d7d4982276bb7c1f7f20a0cb0a8f9776
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-10-22 22:53:47 +0100

    mandatory tasks 2&3 done

---
+++ 3-quick_sort.c
@@ -0,0 +1,81 @@
+#include "sort.h"
+
+/**
+* quick_sort - Sorts an array of integers in ascending order using Quick sort
+* @array: The array to be sorted
+* @size: Number of elements in the array
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	quicksort_recursive(array, 0, size - 1, size);
+	print_array(array, size);
+}
+
+/**
+* quicksort_recursive - Recursive function to perform Quick sort
+* @array: The array to be sorted
+* @start: The low index of the partition
+* @end: The high index of the partition
+* @size: Number of elements in the array
+*/
+void quicksort_recursive(int *array, int start, int end, size_t size)
+
+{
+	if (start < end)
+	{
+		int partition_index = lomuto_partition(array, start, end);
+
+		quicksort_recursive(array, start, partition_index - 1, size);
+		quicksort_recursive(array, partition_index + 1, end, size);
+		print_array(array, size);
+	}
+}
+
+/**
+* lomuto_partition - Lomuto partition scheme for Quick sort
+* @array: The array to be partitioned
+* @start: The low index of the partition
+* @end: The high index of the partition
+* @size: Number of elements in the array
+* Return: The pivot index
+*/
+int lomuto_partition(int *array, int start, int end)
+{
+
+	int part_index, i, pivot;
+
+	pivot = array[end];
+	part_index = start - 1;
+
+	for (i = start; i < end; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			part_index++;
+			swap(&array[part_index], &array[i]);
+		}
+	}
+	part_index++;
+	swap(&array[part_index], &array[end]);
+	return (part_index);
+}
+
+/**
+* swap - Swaps two integers
+* @a: Pointer to the first integer
+* @b: Pointer to the second integer
+*/
+void swap(int *a, int *b)
+{
+	if (a != b)
+	{
+		int temp = *a;
+
+		*a = *b;
+		*b = temp;
+	}
+}
--- sort.h
+++ sort.h
@@ -1,16 +1,16 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
 	const int n;
@@ -21,10 +21,13 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int start, int end);
+void quicksort_recursive(int *array, int start, int end, size_t size);
 void quick_sort(int *array, size_t size);
 
-
-#endif
+#endif /* SORT_H */
