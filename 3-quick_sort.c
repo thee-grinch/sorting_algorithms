c16d6c1d10b172d8439181bdf0e14f6f94e23be7
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-10-23 20:43:35 +0100

    fix logic errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,7 +12,6 @@ void quick_sort(int *array, size_t size)
 		return;
 	}
 	quicksort_recursive(array, 0, size - 1, size);
-	print_array(array, size);
 }
 
 /**
@@ -27,11 +26,10 @@ void quicksort_recursive(int *array, int start, int end, size_t size)
 {
 	if (start < end)
 	{
-		int partition_index = lomuto_partition(array, start, end);
+		int partition_index = lomuto_partition(array, start, end, size);
 
 		quicksort_recursive(array, start, partition_index - 1, size);
 		quicksort_recursive(array, partition_index + 1, end, size);
-		print_array(array, size);
 	}
 }
 
@@ -40,28 +38,38 @@ void quicksort_recursive(int *array, int start, int end, size_t size)
 * @array: The array to be partitioned
 * @start: The start index of the partition
 * @end: The end index of the partition
+* @size: Number of elements in the array
 * Return: The pivot index
 */
-int lomuto_partition(int *array, int start, int end)
+int lomuto_partition(int *array, int start, int end, size_t size)
 {
-
 	int part_index, i, pivot;
 
 	pivot = array[end];
-	part_index = start - 1;
+	part_index = start;
 
 	for (i = start; i < end; i++)
 	{
 		if (array[i] <= pivot)
 		{
+			if (i != part_index)
+			{
+				swap(&array[part_index], &array[i]);
+				print_array(array, size);
+			}
 			part_index++;
-			swap(&array[part_index], &array[i]);
 		}
 	}
-	part_index++;
-	swap(&array[part_index], &array[end]);
+
+	if (array[part_index] != array[end])
+	{
+		swap(&array[part_index], &array[end]);
+		print_array(array, size);
+	}
+
 	return (part_index);
 }
+
 
 /**
 * swap - Swaps two integers
