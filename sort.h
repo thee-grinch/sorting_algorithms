c9bb8f2a7c5e30dfa0c13c0aeb104a2632c0e3ee
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-10-23 20:46:31 +0100

    modify header file

--- sort.h
+++ sort.h
@@ -26,7 +26,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
-int lomuto_partition(int *array, int start, int end);
+int lomuto_partition(int *array, int start, int end, size_t size);
 void quicksort_recursive(int *array, int start, int end, size_t size);
 void quick_sort(int *array, size_t size);
 
